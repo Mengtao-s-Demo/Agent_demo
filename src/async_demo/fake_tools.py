cdf9234fd0113a6cf2fe61f8cf6439b02467546d
lumengtao <lumengtao@live.com>
2026-07-23 15:19:45 +0800

    cancel async

--- src/async_demo/fake_tools.py
+++ src/async_demo/fake_tools.py
@@ -16,14 +16,37 @@ async def download(name:str,download_time:int = 5):
     except TimeoutError as e:
         print(f"执行任务超时，下载：{name}失败")
         return f"下载 : {name}, 失败！"
+    except asyncio.CancelledError as c:
+        print(f"任务被取消：{repr(c)}，name: {name}")
 
 
 async def main():
 
-    await asyncio.gather(
-    download("A"), ## 正常下载
-    download("B", 11), ## 超时
-    download("text_error") ## 抛出异常
-    )
+    try:
+        await asyncio.gather(
+            download("A"), ## 正常下载
+            download("B", 11), ## 超时
+            download("text_error") ## 抛出异常
+            )
+    except Exception as e:
+        print(f"gather 错误：{repr(e)}")
 
 asyncio.run(main()) # 生成一个事件循环
+
+## 任务取消
+
+async def task_cancel():
+    task = asyncio.create_task(download("A",8))
+
+    await asyncio.sleep(4)
+
+    ## 取消任务，发送取消消息
+    task.cancel() 
+    print("取消任务")
+
+    try:
+        await task ## 等待任务最终完成
+    except asyncio.CancelledError :
+        print(task.cancelled())
+
+asyncio.run(task_cancel())
