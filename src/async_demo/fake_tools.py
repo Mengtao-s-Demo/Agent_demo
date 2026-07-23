6a905024c3c88514377d4a83faf42cc09056678f
lumengtao <lumengtao@live.com>
2026-07-23 14:20:12 +0800

    async

--- src/async_demo/fake_tools.py
+++ src/async_demo/fake_tools.py
@@ -11,7 +11,7 @@ async def download(name:str,download_time:int = 5):
 
     try:
         async with asyncio.timeout(timeout):
-            await asyncio.sleep(download_time)
+            await asyncio.sleep(download_time) # await阻塞当前协程，交给asyncio.sleep这个协程
             print(f"下载成功： {name}")
     except TimeoutError as e:
         print(f"执行任务超时，下载：{name}失败")
@@ -20,12 +20,10 @@ async def download(name:str,download_time:int = 5):
 
 async def main():
 
-        await asyncio.gather(
-        download("A"), ## 正常下载
-        download("B", 11), ## 超时
-        download("text_error") ## 抛出异常
-        )
+    await asyncio.gather(
+    download("A"), ## 正常下载
+    download("B", 11), ## 超时
+    download("text_error") ## 抛出异常
+    )
 
-
-
-asyncio.run(main())
+asyncio.run(main()) # 生成一个事件循环
